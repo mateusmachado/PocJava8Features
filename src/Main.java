2e83672aa381dcaab14bd9023d530e101b41d9b2
mateusmachado <mmachado.csp@gmail.com>
2016-06-15 00:43:04 -0300

    main class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 	public static void main(String[] args) {
-	  
+		List<String> lines = Arrays.asList("spring", "node", "others");
+		List<String> result = lines.stream().filter(line -> !"others".equals(line))
+		.collect(Collectors.toList());
+		result.forEach(System.out::println);
 	}
 }
