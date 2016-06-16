77bcff5269b3616518a95aed47e4d3cc48aaf051
mmachado.csp@gmail.com <mmachado.csp@gmail.com>
2016-06-16 14:32:33 -0300

    refactoring

--- src/ex2/PersonMain.java
+++ src/ex2/PersonMain.java
@@ -2,34 +2,33 @@ package ex2;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class PersonMain {
 
 	public static void main(String[] args) {
-		List<Person> persons = Arrays.asList
-			   (new Person("Joao", "Silva", "POA", 2),
-				new Person("Maria", "Souza", "SP", 4),
-				new Person("Nina", "Silva", "POA", 18),
+		List<Person> people = Arrays.asList(new Person("Joao", "Silva", "POA", 2),
+				new Person("Maria", "Souza", "SP", 4), new Person("Nina", "Silva", "POA", 18),
 				new Person("Jown", "Snow", "RJ", 24));
 
-		// Average Peoples
-		double average = persons.stream()
-				.mapToInt(Person::getAge)
-				.average()
-				.getAsDouble();
-		 System.out.println(average);
+		System.out.println("Average age of people: " + averageAgeOfPeople(people));
+		System.out.println("Name Peoples Poa: " + namePeoplesPOA(people));
+		System.out.println("Count last names: " + countLastNames(people));
+	}
 
-		// Name peoples POA
-		List<String> namePeoplesPOA = persons.stream()
-				.filter(i -> i.getCidade().equalsIgnoreCase("POA"))
-				.map(p -> p.getFirstName())
+	private static Map<String, Long> countLastNames(List<Person> people) {
+		Map<String, Long> counted = people.stream()
+				.collect(Collectors.groupingBy(Person::getLastName, Collectors.counting()));
+		return counted;
+	}
+
+	private static List<String> namePeoplesPOA(List<Person> people) {
+		return people.stream().filter(i -> i.getCidade().equalsIgnoreCase("POA")).map(p -> p.getFirstName())
 				.collect(Collectors.toList());
-		System.out.println(namePeoplesPOA);
+	}
 
-		
-		
-		
-
+	private static double averageAgeOfPeople(List<Person> people) {
+		return people.stream().mapToInt(Person::getAge).average().getAsDouble();
 	}
 }
